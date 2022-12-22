6dc8471b3b3cfae16e459097501bfa4b5b6fb755
timpezdirc <Tim.Pezdirc@gmail.com>
2022-12-22 19:43:01 +0100

    Create styles.css and style the site

--- javascript.js
+++ javascript.js
@@ -7,31 +7,20 @@ function getComputerChoice(choice) {
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
-        return "You win! Rock beats scissors!";
+        return "You win! The computer chose scissors!";
     } else if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
-        return "You lose! Paper beats rock!";
+        return "You lose! The computer chose rock!";
     } else if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
-        return "You lose! Scissors beat paper!";
+        return "You lose! SThe computer chose paper!";
     } else if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-        return "You win! Paper beats rock!";
+        return "You win! The computer chose rock!";
     } else if(playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
-        return "You lose! Rock beats scissors!";
+        return "You lose! The computer chose scissors!";
     } else if(playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
-        return "You win! Scissors beat paper;"
+        return "You win! The computer chose paper!;"
     } else {
         return "Draw!";
     }
 }
 
-function game(numberOfRounds) {
-    for(let i = 0; i < numberOfRounds; i++) {
-        const playerChoice = prompt("Rock, paper or scissors?");
-        console.log(playerChoice);
-        const computerChoice = getComputerChoice(choose);
-        console.log(computerChoice);
 
-        alert(playRound(playerChoice, computerChoice));
-    }
-}
-
-game(5);
